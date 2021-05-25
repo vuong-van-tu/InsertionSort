dc11bfc16ac2d74b5da85e04dcfa40360849f454
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-25 17:25:19 +0700

    cmt

---
+++ src/InsertionSort.java
@@ -0,0 +1,21 @@
+public class InsertionSort {
+    static int[] list = {1, 9, 3,5,6,8};
+    public static void insertionSort(int[] array){
+        int pos, x;
+        for(int i = 1; i < array.length; i++){ //đoạn array[0] đã sắp xếp
+            x = array[i];
+            pos = i;
+            while(pos > 0 && x < array[pos-1]){
+                array[pos] = array[pos-1]; // đổi chỗ
+                pos--;
+            }
+            array[pos] = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        insertionSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
